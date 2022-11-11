a8cc7a3b50dd209388b55c0cba1167e05c67e381
welderdefender <welderdefender@yandex.ru>
2022-11-11 17:56:25 +0300

    solution: writeFunctionWhichConvertsInputStringToUppercase

---
+++ Codewars/beginner/writeFunctionWhichConvertsInputStringToUppercase.java
@@ -0,0 +1,11 @@
+package beginner;
+
+/**
+ * Ссылка на задачу: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
+ */
+
+public class writeFunctionWhichConvertsInputStringToUppercase {
+    public static String MakeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
