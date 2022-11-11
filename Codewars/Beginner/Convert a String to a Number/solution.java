a7afc6fc444451ab41ae23a57e5feffcf36dcb23
welderdefender <welderdefender@yandex.ru>
2022-11-11 16:10:35 +0300

    solution

--- Codewars/Beginner/Convert a String to a Number/solution.java
+++ Codewars/Beginner/Convert a String to a Number/solution.java
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/java
+// Ссылка на задачу: https://www.codewars.com/kata/544675c6f971f7399a000e79/train/java
 
 public class StringToNumber {
     public static int stringToNumber(String str) {
---
+++ Codewars/Beginner/Opposite Number/solution.java
@@ -0,0 +1,9 @@
+// Ссылка на задачу: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/java
+
+public class Kata
+{
+    public static int opposite(int number)
+    {
+        return number * -1;
+    }
+}
