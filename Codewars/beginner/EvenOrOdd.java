40fa5a06aae1fd70acc9bad5f46046dec2041a39
welderdefender <welderdefender@yandex.ru>
2022-11-14 18:43:16 +0300

    solution

---
+++ Codewars/beginner/EvenOrOdd.java
@@ -0,0 +1,11 @@
+package beginner;
+
+/**
+ * Ссылка на задачу: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+ */
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+}
