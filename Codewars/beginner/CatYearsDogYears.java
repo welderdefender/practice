60308dda8c07b7ffff8f420a4ee84a0d23c8fdc3
welderdefender <welderdefender@yandex.ru>
2022-11-18 16:17:58 +0300

    solution

---
+++ Codewars/beginner/CatYearsDogYears.java
@@ -0,0 +1,42 @@
+package beginner;
+
+/*
+I have a cat and a dog.
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+
+Ссылка на задачу: https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/java
+ */
+
+public class CatYearsDogYears {
+    public static int[] humanYearsCatYearsDogYears(final int humanYears) {
+        int catYears = 0;
+        int dogYears = 0;
+        for (int i = 0; i < humanYears; i++) {
+            if (humanYears == 1) {
+                catYears = 15;
+                dogYears = 15;
+            }
+            if (humanYears > 1) {
+                catYears = 24 + (4 * (humanYears - 2));
+                dogYears = 24 + (5 * (humanYears - 2));
+            }
+        }
+        return new int[]{humanYears, catYears, dogYears};
+    }
+}
--- Codewars/beginner/WriteFunctionWhichConvertsInputStringToUppercase.java
+++ Codewars/beginner/WriteFunctionWhichConvertsInputStringToUppercase.java
@@ -6,7 +6,7 @@ Write a function which converts the input string to uppercase.
 Ссылка на задачу: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/java
  */
 
-public class writeFunctionWhichConvertsInputStringToUppercase {
+public class WriteFunctionWhichConvertsInputStringToUppercase {
     public static String MakeUpperCase(String str) {
         return str.toUpperCase();
     }
