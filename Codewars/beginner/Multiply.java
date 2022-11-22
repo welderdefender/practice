c6de0f37813b302d6c33c9b9fadfc0dcfb735895
welderdefender <welderdefender@yandex.ru>
2022-11-22 16:03:28 +0300

    solution

---
+++ Codewars/beginner/Multiply.java
@@ -0,0 +1,13 @@
+package beginner;
+
+/*
+This code does not execute properly. Try to figure out why.
+
+Ссылка на задачу: https://www.codewars.com/kata/50654ddff44f800200000004/train/java
+ */
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
