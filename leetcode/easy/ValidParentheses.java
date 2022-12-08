a100384892626554b42aa345d0681123f1b1a9bf
welderdefender <welderdefender@yandex.ru>
2022-12-08 17:50:37 +0300

    solution

--- leetcode/easy/ValidParentheses.java
+++ leetcode/easy/ValidParentheses.java
@@ -33,7 +33,7 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(')
                 stack.push(')');
