1e56699f15cd693cc0c1fd3032b17e8d5dd4e2da
welderdefender <welderdefender@yandex.ru>
2022-12-26 12:10:56 +0300

    solution

---
+++ leetcode/easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,51 @@
+package easy;
+
+/*
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+
+1 <= n <= 45
+
+Ссылка на задачу: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode current = head;
+        while (current.next != null) {
+            if (current.val == current.next.val)
+                current.next = current.next.next;
+            else current = current.next;
+        }
+        return head;
+    }
+}
