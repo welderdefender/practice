5c9e1fd76f57785810e7ef8bb352ed594385bc45
welderdefender <welderdefender@yandex.ru>
2022-12-29 15:16:03 +0300

    solution

---
+++ algorithms/SelectionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] target) {
+        for (int i = 0; i < target.length - 1; i++) {
+            int left = target[i];
+            for (int j = i + 1; j < target.length; j++) {
+                int right = target[j];
+                if (left > right) {
+                    target[i] = right;
+                    target[j] = left;
+                    left = right;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 2, 10};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr)); // [2, 3, 5, 6, 10]
+    }
+}
